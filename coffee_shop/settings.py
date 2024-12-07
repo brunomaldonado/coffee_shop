00d38e1edfb6f6536ecab96e7a420289c27d1225
brunomaldonado <brunoutj@gmail.com>
2024-12-07 11:29:57 -0600

    add static file

--- coffee_shop/settings.py
+++ coffee_shop/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = "coffee_shop.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {"default": env.db("DJANGO_DB_URL")}
+# DATABASES = {"default": env.db("DJANGO_DB_URL")}
 
 # DATABASES = {
 #   'default': {
@@ -119,12 +119,12 @@ DATABASES = {"default": env.db("DJANGO_DB_URL")}
 #   }
 # }
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
