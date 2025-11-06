2e12460a47f745c996fde4f54491cf9c9dfb1090
brunomaldonado <brunoutj@gmail.com>
2025-11-06 12:38:37 -0600

    delete-footer-blur

--- coffee_shop/urls.py
+++ coffee_shop/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('products.urls')),
+    path('products/', include('products.urls')),
     path("users/", include("users.urls")),
     path("orders/", include("orders.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
